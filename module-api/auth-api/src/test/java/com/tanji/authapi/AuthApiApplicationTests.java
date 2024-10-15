7899160d8a72ddd53f04110868d4e92714577198
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-10-15 15:44:47 +0900

    FEAT : Module Settings and Issue/PR Template

--- module-api/auth-api/src/test/java/com/tanji/authapi/AuthApiApplicationTests.java
+++ module-api/auth-api/src/test/java/com/tanji/authapi/AuthApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tanji.apimodule;
+package com.tanji.authapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ModuleApiApplicationTests {
+class AuthApiApplicationTests {
 
     @Test
     void contextLoads() {
---
+++ module-api/test-api/src/test/java/com/tanji/testapi/TestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanji.testapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
