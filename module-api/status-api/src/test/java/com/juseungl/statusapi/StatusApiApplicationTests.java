5722169c2575a8b645dcef5fa3f27d826f423082
JuseungL <juseung0619@gmail.com>
2024-11-07 21:10:15 +0900

    FEAT : Tanji Status Module & TanjiStatusDomainModuleSetting - close #19

---
+++ module-api/status-api/src/test/java/com/juseungl/statusapi/StatusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juseungl.statusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- module-domain/domain-rds/src/main/java/com/tanji/domainrds/domains/member/domain/Member.java
+++ module-domain/domain-rds/src/main/java/com/tanji/domainrds/domains/member/domain/Member.java
@@ -20,7 +20,6 @@ public class Member extends BaseTimeEntity {
     private Long id;
 
     @Column(nullable = false)
-
     private String provider;
 
     @Column(nullable = false, name = "social_id")
