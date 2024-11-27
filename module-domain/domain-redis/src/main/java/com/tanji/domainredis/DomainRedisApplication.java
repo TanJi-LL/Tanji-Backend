c0aba3685cdefca5591bfd89401c57fcab3c2a52
JuseungL <juseung0619@gmail.com>
2024-11-27 20:17:58 +0900

    first commit

--- module-domain/domain-redis/src/main/java/com/tanji/domainredis/DomainRedisApplication.java
+++ module-domain/domain-redis/src/main/java/com/tanji/domainredis/DomainRedisApplication.java
@@ -2,8 +2,10 @@ package com.tanji.domainredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.tanji")
 public class DomainRedisApplication {
 
     public static void main(String[] args) {
